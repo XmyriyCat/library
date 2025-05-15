ebe7d9de3f3d381d4bd30309973dbff1dce860e8
Aleshkevich Pavel <aleskevic79@gmail.com>
2025-05-15 17:08:15 +0300

    added Dockerfile for library.client && updated compose.yaml

--- library.client/src/api/ApiEndpoints.js
+++ library.client/src/api/ApiEndpoints.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080/api'; // API base URL is here
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL; // Base api URL is here
 
 export const ApiEndpoints = {
   Author: {
